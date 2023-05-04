efa03881023b7db8ee1e473d3342769bccc155f1
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2023-05-04 14:12:58 +0100

    Implemented + unit-tested graph sampling and PositionalEncodings

--- models.py
+++ models.py
@@ -1,9 +1,57 @@
 """Definition of the Chroma Model. The model takes in Noisy Coordinates [B, N, 4, 3] of the backbone atoms and
 outputs Denoised Coordinates [B, N, 4, 3]. The entire model is end-to-end trainable.
 The main components include:
-    1. Graph Sampling to create a random graph
+    1. Graph Sampling to create a random graph (handled by protein_graph.py module)
     2. Graph Featurization
     3. Graph Neural Network Layer
     4. Inter-residue Geometry Prediction
     5. Backbone Solver
+This module includes all components that are trainable.
 """
+# Dependencies
+import flax
+import jax
+from flax import linen as nn
+
+
+# from typing import i
+
+# Methods for featurization
+# class PositionalEncodings
+# class ProteinFeatures
+
+# noinspection PyAttributeOutsideInit
+class PositionalEncodings(nn.Module):
+    """A module that implements relative positional encodings as edge features. Given an edge between two residues i
+    and j, the relative positional encoding encodes the distance between i and j in the primary amino acid sequence.
+    In the ProteinMPNN work, the relative positional encoding was capped at ±32 residues.
+
+    num_embeddings: the number of positional embeddings
+    max_relative_features: the cap for distance in primary sequence, defaults to 32
+    """
+    num_embeddings: int
+    max_relative_features: int = 32
+
+    @nn.compact
+    def __call__(self, offset, mask):
+        """
+        Args:
+            offset: array of shape [B, N_res, K] encoding the primary sequence offset
+            mask: the binary feature array of shape [B, N_res, K] encoding whether the i and j residues forming the edge
+            are in the same chain or in different chains. An entry is equal to 1.0 if they are from the same chain, 0.0
+            if not. An additional token is added if residues are in different chains (hence the +1).
+        Returns:
+            positional encodings of shape [B, N_res, K, num_embeddings]
+        (unit-tested)"""
+        d = jax.lax.clamp(x=offset + self.max_relative_feature,
+                          min=0, max=2*self.max_relative_feature)*mask + (1-mask)*(2*self.max_relative_feature+1)
+        d_onehot = jax.nn.one_hot(d, 2*self.max_relative_feature+1+1)  # d_onehot.shape == (B, N, K, 32*2+1+1)
+        # input features size: 2*max_relative_feature+1+1
+        # output features size: num_embeddings
+        E = nn.Dense(self.num_embeddings)(d_onehot)  # Embed with trainable weights
+        return E
+
+
+class ProteinFeatures(nn.Module):
+    """A module that """
+    pass
--- protein_graph.py
+++ protein_graph.py
@@ -1,6 +1,5 @@
-"""Random graph generation and featurization for protein graphs. The Chroma architecture uses random graph neural
-networks to constrain computational costs to O(N) for a system of N atoms. This module defines the random graph
-generation and graph featurization processes."""
+"""Random graph generation for protein graphs. The Chroma architecture uses random graph neural networks to constrain
+computational costs to O(N) for a system of N atoms. This module defines the random graph generation process. """
 import jax.numpy as jnp
 import jax
 import numpy as np
@@ -53,6 +52,7 @@ def sample_random_graph(key, backbone_coordinates, temperature=1.0) -> jnp.array
         The elements of the array encode residue indices. For instance, topology[i] = [0, 5, ..., 231] suggests that
         the (i + 1)th residue in the protein is connected to the (0+1)st, (5+1)th, and (231 + 1)nd residues. The actual
         residues in the protein are indexed starting from 0 when represented in arrays.
+    TODO: measure exact probabilities for cubic sampling
     """
     ca_coordinates = extract_carbon_alpha_coordinates(backbone_coordinates)
     distances = get_internode_distances(ca_coordinates)  # distances.shape == (N_res, N_res)
@@ -65,44 +65,71 @@ def sample_random_graph(key, backbone_coordinates, temperature=1.0) -> jnp.array
     # Sample rest with inverse cubic distance (need careful measurements)
     # Instead of sorted distances, I can simply mask those with a large value so that they will never be chosen.
     # This way, I avoid this sorting nonsense
-    # TODO: find a way to implement a custom put_along_axis method that is jittable
     # If I find a way to perform this masking without
-    distances = jnp.put_along_axis(distances, top_k_nearest_neighbours, jnp.inf, axis=1)  # mask with infinite distances
+    distances = custom_put_along_axis(distances, top_k_nearest_neighbours, jnp.inf, axis=1)  # mask with inf distances
     remaining_distances = distances[:, k_nearest:]
     uniform_noise = jax.random.uniform(key, shape=remaining_distances.shape, minval=0.0, maxval=1.0)
-    gumbel_perturbed = (1.0/temperature)*(-3*jnp.log(remaining_distances)) - jnp.log(-jnp.log(uniform_noise))  # TopK
-    # Gumbel-Max Trick (See Kool et al. 2019 for details)
+    gumbel_perturbed = (1.0 / temperature) * (-3 * jnp.log(remaining_distances)) - jnp.log(
+        -jnp.log(uniform_noise))  # TopK Gumbel-Max Trick (See Kool et al. 2019 for details)
     gumbel_perturbed_argsorted = jnp.argsort(gumbel_perturbed, axis=1)
     top_k_cubic_samples = gumbel_perturbed_argsorted[:, -k_cubic:] + 20  # top_k_cubic indices
 
     return jnp.concatenate((top_k_nearest_neighbours, top_k_cubic_samples), axis=1)
 
 
-def cubic_sample_random_graph(key, backbone_coordinates, temperature=1.0) -> jnp.array:
-    """This method samples a stochastic graph topology given backbone coordinates of a protein. The sampling follows
-    the conventions in the Chroma Paper: the top 20 nearest neighbours + 40 inverse cubic distance attachment
-    propensity. For details, see Algorithm 1 and Supplementary Figure 3 in the Chroma Paper.
-    Args:
-        key: random PRNG key
-        backbone_coordinates: backbone coordinates for the protein of shape [N_res, 4, 3]. The backbone atoms should be
-        arranged in order (N, CA, C, O)
-        temperature: temperature for sampling (used as softmax temperature)
-    Returns:
-        a graph topology represented by an array of shape [N_res, K] where K = 60, corresponding to the 60 edges.
-        The elements of the array encode residue indices. For instance, topology[i] = [0, 5, ..., 231] suggests that
-        the (i + 1)th residue in the protein is connected to the (0+1)st, (5+1)th, and (231 + 1)nd residues. The actual
-        residues in the protein are indexed starting from 0 when represented in arrays.
+def custom_put_along_axis(arr, indices, values, axis):
+    """This method implements np.put_along_axis function in Jax. It would be much easier if Jax simply
+    implemented this function.
+    Essentially copied the code from:
+    https://github.com/numpy/numpy/blob/4adc87dff15a247e417d50f10cc4def8e1c17a03/numpy/lib/shape_base.py#L29
+    Parameters
+    ----------
+    arr : ndarray (Ni..., M, Nk...)
+        Destination array.
+    indices : ndarray (Ni..., J, Nk...)
+        Indices to change along each 1d slice of `arr`. This must match the
+        dimension of arr, but dimensions in Ni and Nj may be 1 to broadcast
+        against `arr`.
+    values : array_like (Ni..., J, Nk...)
+        values to insert at those indices. Its shape and dimension are
+        broadcast to match that of `indices`.
+    axis : int
+        The axis to take 1d slices along. If axis is None, the destination
+        array is treated as if a flattened 1d view had been created of it.
+
     """
-    ca_coordinates = extract_carbon_alpha_coordinates(backbone_coordinates)
-    distances = get_internode_distances(ca_coordinates)  # distances.shape == (N_res, N_res)
-    k_cubic = 40
 
-    # Sample with inverse cubic attachment model
-    uniform_noise = jax.random.uniform(key, shape=distances.shape, minval=0.0, maxval=1.0)
-    gumbel_perturbed = (1.0/temperature)*(-3*jnp.log(distances)) - jnp.log(-jnp.log(uniform_noise))  # TopK
-    # Gumbel-Max Trick (See Kool et al. 2019 for details)
-    gumbel_perturbed_argsorted = jnp.argsort(gumbel_perturbed, axis=1)
-    top_k_cubic_samples = gumbel_perturbed_argsorted[:, -k_cubic:]
-    return top_k_cubic_samples
+    def _make_along_axis_idx(in_arr_shape, in_indices, in_axis):
+        # compute dimensions to iterate over
+        if not np.core.numeric.issubdtype(in_indices.dtype, np.core.numeric.integer):
+            raise IndexError("`indices` must be an integer array")
+        if len(in_arr_shape) != in_indices.ndim:
+            raise ValueError("`indices` and `arr` must have the same number of dimensions")
+        shape_ones = (1,) * in_indices.ndim
+        dest_dims = list(range(in_axis)) + [None] + list(range(in_axis + 1, in_indices.ndim))
 
-# Further methods for featurization
+        # build a fancy index, consisting of orthogonal aranges, with the
+        # requested index inserted at the right location
+        fancy_index = []
+        for dim, n in zip(dest_dims, in_arr_shape):
+            if dim is None:
+                fancy_index.append(in_indices)
+            else:
+                ind_shape = shape_ones[:dim] + (-1,) + shape_ones[dim + 1:]
+                fancy_index.append(np.core.numeric.arange(n).reshape(ind_shape))
+
+        return tuple(fancy_index)
+
+    # normalize inputs
+    if axis is None:
+        arr = arr.flat
+        axis = 0
+        arr_shape = (len(arr),)  # flatiter has no .shape
+    else:
+        # axis = normalize_axis_index(axis, arr.ndim)
+        arr_shape = arr.shape
+
+    # use the fancy index
+    arr = arr.at[tuple(_make_along_axis_idx(arr_shape, indices, axis))].set(values)
+    return arr
+
