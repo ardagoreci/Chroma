4ec713af0aee1b44b61e4403728f051a76c49a68
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2023-05-13 14:59:39 +0100

    Diffusion without normalization

--- polymer.py
+++ polymer.py
@@ -11,81 +11,76 @@ import jax
 import jax.numpy as jnp
 
 
-def diffuse(key, coordinates, timestep, rg_confined=True, mode='pile-of-globs'):
+@jax.jit
+def diffuse(noise, coordinates, timestep, rg_confined=True, mode='pile-of-globs'):
     """This method diffuses the coordinates of the polymer up to the given time. The diffusion
     is integrated forward in time according to the following formula:
     x_t = sqrt(alpha_t)*x0 + sqrt(1-alpha_t)*matmul(R,z) where z follows multivariate normal.
     Args:
-        key: the random PRNG key for jax.
+        noise: noise (z) that follows z ~ N(0, I) of shape [N_atoms, 3]
         coordinates: a matrix of size [N, B ,3] where N is the number of residues, B is the number of backbone atoms
         timestep: the timestep between [0, 1] where 0 is the data and 1 is noise.
         rg_confined: whether to use to Radius of Gyration confined (Covariance model #2) in Chroma paper, default True
         mode: the rg_confined mode has a 'pile-of-globs' and 'glob-of-globs' mode
+    Returns:
+        noised coordinates of shape [N, B, 3]
     TODO: get the rg_confined and mode parameters from the config file
+    TODO: implement mean deflation
     (unit-tested)"""
     # Reshape to (N_atoms, 3)
     N, B, _ = coordinates.shape
-    coordinates = coordinates.reshape((N * B, 3))
+    x0 = coordinates.reshape((N * B, 3))
+
     # Get alpha_t from diffusion schedule
     alpha_t = log_snr_schedule(timestep)
 
-    # Normalize coordinates
-    # x0 = coordinates
-    x0, (mean, scale) = normalize_coordinates(coordinates)  # x0 needs to be from a Multivariate Gaussian,
-    # so normalization
-    radius_g = jnp.linalg.norm(scale)  # this is equal to Rg
     if rg_confined:
         # Scaling a and computing b
-        a = 3.8 / radius_g  # # 'segment length' = 3.8 Angstroms, which becomes scaled with norm of scale
-        b = compute_b(N, B, a, radius_g)  # N (res) and B (atoms per res) is input to compute_b function
-        rz = rg_confined_covariance(key, x0, a, b)
+        a = 2.1939  # # 'segment length' = 3.8 Angstroms, which becomes scaled with sqrt(3)
+        b = compute_b(N, B, a)  # N (res) and B (atoms per res) is input to compute_b function
+        rz = rg_confined_covariance(noise, n_atoms=x0.shape[0], a=a, b=b)
     else:  # use ideal-chain diffusion
+        radius_g = compute_radius_of_gyration(x0)
         gamma = math.sqrt((radius_g ** 2) * 2 / N)
-        rz = ideal_covariance(key, x0, gamma=gamma, delta=0.0)  # parameters from config file
+        rz = ideal_covariance(noise, n_atoms=x0, gamma=gamma, delta=0.0)  # parameters from config file
 
     # Diffusion Step
-    x_t = jnp.sqrt(alpha_t) * x0 + jnp.sqrt(1 - alpha_t) * rz
+    x_t = jnp.sqrt(alpha_t) * x0 + jnp.sqrt(1 - alpha_t) * rz  # * scale
 
     # Mean deflation operation TODO: implement this
     if rg_confined:
         # deflate_mean()
         pass
+    # Reshape to [N, B, 3]
+    noised_coordinates = x_t.reshape((N, B, 3))
+    return noised_coordinates
 
-    # Coordinates back to Angstroms
-    diffused_coordinates = x_t * scale + mean
-    return diffused_coordinates.reshape((N, B, 3))
 
-
-def ideal_covariance(key, x0, gamma, delta):
+def ideal_covariance(z, n_atoms, gamma, delta):
     """This method computes the matmul(R, z) for the ideal chain covariance model. For the formula, see section C.2
     'Covariance model #1: Ideal Chain' in Chroma Paper.
     Args:
-        key: Jax random key
-        x0: coordinates of atoms with shape [n_atoms, 3]
+        z: noise samples from z ~ N(0, I) of shape x0.shape
+        n_atoms: number of atoms in chain
         gamma: length scale of the chain
         delta: amount of allowed translational noise about the origin
     (unit-tested)"""
-    n_atoms, _ = x0.shape
-    z = jax.random.normal(key, shape=x0.shape)  # z ~ N(0, I)
     cu_z = gamma * jnp.cumsum(z, axis=0)  # cumulative z
-    # cu_z.shape == (n_atoms*3,)
     constant_terms = delta * cu_z[0] - (1 / n_atoms) * (jnp.sum(cu_z))
-    return cu_z + constant_terms  # jnp.reshape(cu_z, x0.shape) + constant_terms
+    return cu_z + constant_terms
 
 
-@jax.jit
-def rg_confined_covariance(key, x0, a, b):
+def rg_confined_covariance(z, n_atoms, a, b):
     """This method computes the matmul(R, z) for the Rg-confined, linear-time Polymer MVNs. For the construction of
     the R matrix, see section C.3 'Covariance model #2: Rg-confined, linear-time Polymer MVNs' in the Chroma Paper.
     Args:
-        key: Jax random key
-        x0: coordinates of atoms with shape [n_atoms, 3]
+        z: noise samples from z ~ N(0, I) of shape x0.shape
+        n_atoms: number of atoms
         a: a global scale parameter setting the 'segment length' of polymer
         b: a 'decay' parameter which sets the memory of the chain to fluctuations
-    (unit-tested)"""
-    n_atoms, _ = x0.shape
-    z = jax.random.normal(key, shape=x0.shape)  # z ~ N(0, I)
+    Returns:
 
+    (unit-tested)"""
     # Construct Rg confined covariance matrix
     b_vec = b ** (jnp.arange(n_atoms))
 
@@ -113,7 +108,7 @@ def log_snr_schedule(timestep):
     return jnp.exp(-jnp.e ** (-4) - 10 * (timestep ** 2))
 
 
-def compute_b(N, B, a, scale_norm):
+def compute_b(N, B, a):
     """This method computes b given the global scaling factor a. See section C.3.2 in Chroma Paper for the derivation
     of the formula. The derivation makes a power law approximation for the radius of gyration in the form r*(N**v)
     where r is a prefactor and v is the Flory coefficient. The values of r and v are taken from Tanner (2016) in the
@@ -125,30 +120,16 @@ def compute_b(N, B, a, scale_norm):
         N: number of residues in the protein
         B: number of atoms per residue
         a: global scale factor that determines the 'segment length' of the polymer
-        scale_norm: norm for the standard deviations of xyz axes
-    (unit-tested)"""
+    """
     # (See Tanner, 2016)
     n_atoms = N * B
-    r = 2.0 / (B ** 0.4)
-    r = r / scale_norm  # r is an experimentally determined prefactor with value 2.0 Angstroms but is
-    # scaled to this value when n_atoms are used
+    r = (2.0 / jnp.sqrt(3)) / (B ** 0.4)  # r is an experimentally determined prefactor with value 2.0 Angstroms but is
+    # scaled to this value when n_atoms are used. Also, the segment length 'a' of 3.8 Angstroms is scaled with
+    # sqrt(3) so r has to be scaled with the same number to keep b invariant
     v = 0.4  # narrow range between 0.38-0.40, taken to be 0.40 (See Tanner, 2016)
     b_effective = 3 / n_atoms + (n_atoms ** (-v)) * jnp.sqrt(
         n_atoms ** (2 * v - 2) * (n_atoms ** 2 + 9) - ((a ** 2) / (r ** 2)))
     return b_effective
-
-
-def normalize_coordinates(coordinates):
-    """This method normalizes coordinates based by centering them at the origin and dividing by one standard deviation
-    of each axis of xyz. This was chosen because the norm of scale is equal to the Radius of Gyration, which simplifies
-    things.
-    Args:
-        coordinates: matrix of size (n_atoms, 3) containing the atom coordinates
-    (unit-tested)"""
-    mean = jnp.average(coordinates, axis=0)
-    scale = jnp.std(coordinates, axis=0)
-    n_coordinates = ((coordinates - mean) / scale)
-    return n_coordinates, (mean, scale)
 
 
 def deflate_mean(x, xi):
